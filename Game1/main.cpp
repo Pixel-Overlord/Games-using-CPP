22c1f2776089f4fa2f82f6a2170e374263bd6988
Satish Jha <jha.satish0210@gmail.com>
2026-07-02 22:04:30 +0530

    Added ball element.

--- Game1/main.cpp
+++ Game1/main.cpp
@@ -10,9 +10,19 @@ int main()
     paddle.setPosition({ 350.f, 560.f });
     paddle.setFillColor(sf::Color::White);
 
+    // ball
+    sf::CircleShape ball( 10.f );
+    paddle.setPosition({ 390.f, 290.f });
+    paddle.setFillColor(sf::Color::White);
+
     // Game Loop
     while (window.isOpen())
     {
+		// Timer for starting the ball
+        sf::Clock timer;
+        bool ballStarted = false;
+        float ballSpeed = 4.f;
+
         // Event loop
         while (const std::optional<sf::Event> event = window.pollEvent())
         {
@@ -30,6 +40,7 @@ int main()
             }
         }
 
+		// Paddle movement
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
         {
             paddle.move({ -0.5f, 0.f });
@@ -40,9 +51,21 @@ int main()
             paddle.move({ 0.5f, 0.f });
         }
 
-        window.clear(sf::Color::Blue);
+		// Start the ball after 5 seconds
+        if (!ballStarted && timer.getElapsedTime().asSeconds() >= 0.5f)
+        {
+            ballStarted = true;
+        }
 
-         // Draw objects here
+        if (ballStarted)
+        {
+            ball.move({ 0.f, ballSpeed });
+        }
+
+        window.clear(sf::Color::Black);
+
+        // Draw objects here
+        window.draw(ball);
 		window.draw(paddle);
 
         window.display();
