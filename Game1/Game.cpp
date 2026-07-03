b038bae7beb33ea74c086c3a7f51bd7590e19842
Satish Jha <jha.satish0210@gmail.com>
2026-07-03 15:45:14 +0530

    Game file skeleton add

---
+++ Game1/Game.cpp
@@ -0,0 +1,30 @@
+#include "Game.h"
+
+// Private functions
+void Game::initVariables()
+{
+}
+
+void Game::initWindow()
+{
+}
+
+// Constructors and Destructors
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
+
+// Functions
+void Game::update()
+{
+
+}
+
+void Game::render()
+{
+
+}
